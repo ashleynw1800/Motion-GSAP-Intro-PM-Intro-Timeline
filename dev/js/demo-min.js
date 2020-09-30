20215b712d2f6f744c2a0a8f191244e414d48e45
ashleynw1800 <ashleynw1800@gmail.com>
2020-09-29 19:00:52 -0700

    Initial commit

---
+++ dev/js/demo-min.js
@@ -0,0 +1,1 @@
+import{gsap}from"gsap";var redSpeed=2,blueSpeed=1.5;gsap.to(".red-box",{duration:redSpeed,x:500,borderRadius:"50px"}),gsap.to(".red-box",{duration:redSpeed,y:500,borderRadius:"100px",delay:redSpeed}),gsap.from(".blue-box",{duration:blueSpeed,x:300,y:300,borderRadius:"50px"}),gsap.to(".blue-box",{duration:blueSpeed,y:100,borderRadius:"200px",delay:blueSpeed});
